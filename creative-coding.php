acd96f0d23831d62c0b4cd1a8600b291475e87a9
tricialeach <tleach@gmail.com>
2017-11-03 22:46:12 -0700

    Add creative coding project page

--- creative-coding.php
+++ creative-coding.php
@@ -13,11 +13,19 @@
 
     <div class="c-projects">
 
-    <p class="fifty"><img src="img/project-earthquake-prep1.jpg" alt="Earthquake prep guide"></p>
-    <p><img src="img/project-earthquake-prep2.jpg" alt="Earthquake prep guide"></p>
-    <p><img src="img/project-earthquake-prep3.jpg" alt="Earthquake prep guide"></p>
-    <p><img src="img/project-earthquake-prep4.jpg" alt="Earthquake prep guide"></p>
-    <p><img src="img/project-earthquake-prep5.jpg" alt="Earthquake prep guide"></p>
+    <p><img src="img/project-creative-coding1.gif" alt="Creative coding"></p>
+    <p><img src="img/project-creative-coding2.gif" alt="Creative coding"><br><br><br><br><br></p>
+    
+    <p>
+    <iframe src="https://player.vimeo.com/video/240512781" width="900" height="506" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <br><br><br><br><br></p>
+
+    <p>
+    <iframe src="https://player.vimeo.com/video/221273646" width="900" height="506" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <br><br><br><br><br></p>
+
+    <iframe height='500' scrolling='no' title='004/100 — CSS Animation Practice' src='//codepen.io/tricialeach/embed/bRYBNO/?height=500&theme-id=0&default-tab=css,result&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'>See the Pen <a href='https://codepen.io/tricialeach/pen/bRYBNO/'>004/100 — CSS Animation Practice</a> by Tricia Leach (<a href='https://codepen.io/tricialeach'>@tricialeach</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>
 
     </div> <!-- end projects container -->
   </main>
